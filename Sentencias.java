436a87b9364dad09d552b8cdc9ceb6eaa0f264dd
Jose Daniel <36455248+softwaredaniel@users.noreply.github.com>
2023-02-08 15:53:29 -0500

    Create sentencias

---
+++ Sentencias.java
@@ -0,0 +1,18 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int numeroIf=8;
+        if (numeroIf<0){
+      System.out.println("El numero es negativo");
+       }else{
+             System.out.println("El numero es positivo");
+           
+    }
+    int numeroWhile=1;
+     while(numeroWhile<3){
+       numeroWhile++;
+         System.out.println("Entro al bucle while");
+     }  
+    }
+    
+ 
+}
