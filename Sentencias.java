d83bfa0ab98be66fb1180e8f4da0029448cceb87
Jose Daniel <36455248+softwaredaniel@users.noreply.github.com>
2023-02-09 13:38:56 -0500

    Add bucle for and switch in java
    En este ejercicio practicarás las estructuras de control, para ello deberás crear:
    
    Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
    Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
    
    Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
    
    Incrementar el valor de la variable en uno cada vez que se ejecute.
    
    Mostrarlo por pantalla cada vez que se ejecute.
    
    Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
    
    Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
    
    Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.

--- Sentencias.java
+++ Sentencias.java
@@ -2,22 +2,45 @@ public class MyClass {
     public static void main(String args[]) {
         int numeroIf=8;
         if (numeroIf<0){
-      System.out.println("El numero es negativo");
+           System.out.println("El numero es negativo");
        }else{
              System.out.println("El numero es positivo");
            
     }
-    int numeroWhile=1;
-     while(numeroWhile<3){
-       numeroWhile++;
-         System.out.println("Entro al bucle while");
-     } 
-    do{ 
-        numeroWhile++;
-        System.out.println("Entro al bucle while");
-         }
-    while(numeroWhile<3);
+        int numeroWhile=1;
+        while(numeroWhile<3){
+          numeroWhile++;
+          System.out.println("Entro al bucle while");
+        } 
+        do{ 
+          numeroWhile++;
+          System.out.println("Entro al bucle while");
+          }
+        while(numeroWhile<3);
+        for(int numeroFor=0;numeroFor<=3;numeroFor=numeroFor+1)
+        {
+            System.out.println("El numero for es"+numeroFor);
+        }
         
+         String estacion="Primavera";
+      
+      switch(estacion){
+          case "Invierno":
+              System.out.println("Estas en invierno");
+              break;
+          case "Verano":
+               System.out.println("Estas en Verano");
+               break;
+          case "Primavera":
+               System.out.println("Estas en Primavera");
+               break;
+          case "Otoño":
+               System.out.println("Estas en Otoño");
+               break;
+          default:
+               System.out.println("No existe estacion");
+              
+      }
         
     }
     
